ddc270232e41448b9c68f5853df71821488c0f04
Alex Vidyakov <alex.vidyakov@yandex.ru>
2022-11-06 18:22:05 +0300

    chore: подготовить БД

--- src/core/config.py
+++ src/core/config.py
@@ -6,7 +6,7 @@ class Config(BaseSettings):
     host: str = '0.0.0.0'
     port: int = 8000
     database_dsn: PostgresDsn = (
-        'postgresql+asyncpg://admin:admin@localhost:5432/postgres'
+        'postgres://admin:admin@localhost:5432/postgres'
     )
 
     class Config:
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from fastapi.responses import ORJSONResponse
 
 from src.api import router
 from src.core.config import config
+from src.db import register_db
 
 app = FastAPI(
     title=config.project_name,
@@ -11,6 +12,8 @@ app = FastAPI(
 )
 
 app.include_router(router.router)
+
+register_db(app)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host=config.host, port=config.port)
