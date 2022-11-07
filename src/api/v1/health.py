95d63a3e0c163b5e906e14a6a8b06995ee3f2fa3
Alex Vidyakov <alex.vidyakov@yandex.ru>
2022-11-07 06:49:37 +0300

    feat: healthcheck

---
+++ src/api/v1/health.py
@@ -0,0 +1,22 @@
+from http import HTTPStatus
+
+from fastapi import APIRouter, Response
+
+from src.models.link import Link
+
+router = APIRouter(
+    prefix='/health',
+    tags=['health'],
+)
+
+
+@router.get('')
+async def check_health():
+    status = HTTPStatus.OK
+
+    try:
+        await Link.all().count()
+    except OSError:
+        status = HTTPStatus.SERVICE_UNAVAILABLE
+
+    return Response(status_code=status)
--- src/api/v1/router.py
+++ src/api/v1/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from src.api.v1 import links
+from src.api.v1 import health, links
 
 router = APIRouter(prefix='/v1')
 router.include_router(links.router)
+router.include_router(health.router)
