fe705fcc754798cb6eda15488613b7b50134d425
Alex Vidyakov <alex.vidyakov@yandex.ru>
2022-11-13 11:52:53 +0300

    feat: удалить ссылку

--- src/api/v1/links.py
+++ src/api/v1/links.py
@@ -1,6 +1,8 @@
+from http import HTTPStatus
+
 from fastapi import APIRouter, Request
 from fastapi_pagination import Page, paginate
-from starlette.responses import RedirectResponse
+from starlette.responses import RedirectResponse, Response
 
 from src.models.link import Click, Link
 from src.schemas.links import ClickModel, LinkInModel, LinkModel
@@ -19,13 +21,21 @@ async def create_link(link_data: LinkInModel):
 
 @router.get('/{link_id}', response_class=RedirectResponse)
 async def redirect(link_id: int, request: Request):
-    link = await Link.get(id=link_id)
+    link = await Link.get(id=link_id, is_deleted=False)
     await Click.create(link=link, address=':'.join(map(str, request.client)))
     return link.origin
 
 
+@router.delete('/{link_id}')
+async def delete_link(link_id: int):
+    link = await Link.get(id=link_id, is_deleted=False)
+    link.is_deleted = True
+    await link.save()
+    return Response(status_code=HTTPStatus.NO_CONTENT)
+
+
 @router.get('/{link_id}/clicks', response_model=Page[ClickModel])
 async def get_clicks(link_id: int):
-    link = await Link.get(id=link_id)
+    link = await Link.get(id=link_id, is_deleted=False)
     clicks = await ClickModel.from_queryset(link.clicks.all())
     return paginate(clicks)
--- src/models/link.py
+++ src/models/link.py
@@ -9,6 +9,7 @@ from src.models.base import BaseModel
 class Link(BaseModel):
     origin = fields.CharField(max_length=255)
     created_at = fields.DatetimeField(auto_now_add=True)
+    is_deleted = fields.BooleanField(default=False)
 
     def short(self) -> str:
         return urljoin(config.short_url_host, str(self.id))
